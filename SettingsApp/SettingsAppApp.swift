a77b74372e0efafbc1f2db30e85bfcfca9927739
aewols <i0y0j057@gmail.com>
2023-06-04 13:33:43 +0900

    Initial Commit

---
+++ SettingsApp/SettingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsAppApp.swift
+//  SettingsApp
+//
+//  Created by 이연정 on 2023/06/04.
+//
+
+import SwiftUI
+
+@main
+struct SettingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
